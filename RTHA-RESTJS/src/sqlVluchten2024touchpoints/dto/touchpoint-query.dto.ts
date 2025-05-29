ee9e80553e7135e6f7db6b558d3ce9e8fed76b1e
H1cham1 <hichamt312@gmail.com>
2025-05-29 13:57:38 +0200

    Removed unneeded IsOptional() decorator in touchpoint dto

--- RTHA-RESTJS/src/sqlVluchten2024touchpoints/dto/touchpoint-query.dto.ts
+++ RTHA-RESTJS/src/sqlVluchten2024touchpoints/dto/touchpoint-query.dto.ts
@@ -1,6 +1,5 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import {
-    IsOptional,
     IsNumber,
     IsString,
     IsDateString,
@@ -8,87 +7,70 @@ import {
 
 export class TouchpointQueryDto {
     @ApiPropertyOptional()
-    @IsOptional()
     @IsNumber()
     FlightID?: number;
 
     @ApiPropertyOptional()
-    @IsOptional()
     @IsNumber()
     TimetableID?: number;
 
     @ApiPropertyOptional()
-    @IsOptional()
     @IsString()
     FlightNumber?: string;
 
     @ApiPropertyOptional()
-    @IsOptional()
     @IsString()
     TrafficType?: string;
 
     @ApiPropertyOptional()
-    @IsOptional()
     @IsDateString()
     ScheduledLocal?: string;
 
     @ApiPropertyOptional()
-    @IsOptional()
     @IsString()
     AirlineShortname?: string;
 
     @ApiPropertyOptional()
-    @IsOptional()
     @IsString()
     AircraftType?: string;
 
     @ApiPropertyOptional()
-    @IsOptional()
     @IsString()
     Airport?: string;
 
     @ApiPropertyOptional()
-    @IsOptional()
     @IsString()
     Country?: string;
 
     @ApiPropertyOptional()
-    @IsOptional()
     @IsNumber()
     PaxForecast?: number;
 
     @ApiPropertyOptional()
-    @IsOptional()
     @IsString()
     Touchpoint?: string;
 
     @ApiPropertyOptional()
-    @IsOptional()
     @IsDateString()
     TouchpointTime?: string;
 
     @ApiPropertyOptional()
-    @IsOptional()
     @IsNumber()
     TouchpointPax?: number;
 
     @ApiPropertyOptional()
-    @IsOptional()
     @IsDateString()
     ActualLocal?: string;
 
     @ApiPropertyOptional()
-    @IsOptional()
     @IsNumber()
     PaxActual?: number;
 
     @ApiPropertyOptional({ description: 'Aantal resultaten per pagina' })
-    @IsOptional()
     @IsNumber()
     limit?: number;
 
     @ApiPropertyOptional({ description: 'Aantal over te slaan resultaten (offset)' })
-    @IsOptional()
     @IsNumber()
     offset?: number;
 }
